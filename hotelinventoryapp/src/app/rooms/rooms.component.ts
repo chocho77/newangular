e0c81ecc22c567a1e3afb4c73cc7a7ee565b46f0
chavdar77 <chavmom@gmail.com>
2022-11-10 08:04:45 +0200

    Changes to be committed: 	modified:   hotelinventoryapp/src/app/rooms/rooms.component.ts

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomsComponent implements OnInit {
 
+  hotelName = 'Hilton Hotel';
+
   constructor() { }
 
   ngOnInit(): void {
